6aec902ae459bcb80ff7618416bc16cb29e7304c
Joao Almeida <joaofilipe90@gmail.com>
2023-01-21 15:10:03 +0000

    starting

--- creator.py
+++ creator.py
@@ -1,15 +1,18 @@
-from os import listdir, getcwd, mkdir, rmdir
-from os.path import isfile, join, exists
-import glob
 from jinja2 import Template, Environment, FileSystemLoader
 import pandas as pd
-from pathlib import Path
-import numpy as np
-import sys
 import uuid
 import re
 from datetime import datetime
 import hashlib
+from js import document, console
+import asyncio
+import js
+from pyodide import create_proxy
+import io
+from js import document, console, Uint8Array, window, File
+from os import listdir, getcwd, mkdir, rmdir
+from os.path import isfile, join, exists
+
 
 context = {"now": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")}
 
@@ -36,7 +39,7 @@ def hash_id(string):
 env.filters["create_hash_id"] = hash_id
 
 
-def create_from_template(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER):
+def create_from_template(DATA_FILE):
     elements = [
         "AdministrableProductDefinition",
         "Substance",
@@ -50,22 +53,23 @@ def create_from_template(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER):
         "PackagedProductDefinition",
         "Bundle",
     ]
-
+    fileList = document.getElementById("input").files
+    TEMPLATE_FOLDER = "templates/"
+    # OUTPUT_FOLDER = "output/"
     # create temp_folder:
-    print(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER)
+    # print(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER)
+    print(DATA_FILE)
 
-    if TEMPLATE_FOLDER[-1] != "/":
-        TEMPLATE_FOLDER += "/"
-    if OUTPUT_FOLDER[-1] != "/":
-        OUTPUT_FOLDER += "/"
-
+    # print(getcwd())
     temp_folder = getcwd() + "/temp/"
 
     if not exists(temp_folder):
         mkdir(temp_folder)
-    major_name = DATA_FILE.lower().split("/")[-1].split(".")[0].replace(" ", "_")
-    real_output_folder = OUTPUT_FOLDER + major_name + "-ema-automatic/"
-    print(real_output_folder)
+    major_name = (
+        "acme"  # DATA_FILE.lower().split("/")[-1].split(".")[0].replace(" ", "_")
+    )
+    real_output_folder = getcwd() + "output/"
+
     if not exists(real_output_folder):
         mkdir(real_output_folder)
     for sheet in elements:
@@ -142,5 +146,74 @@ def create_from_template(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER):
         t.stream(data=data, **context).dump(real_output_folder + n_file + ".fsh")
 
 
-# if __name__ == "__main__":
-#    create_from_template(DATA_FILE, TEMPLATE_FOLDER, OUTPUT_FOLDER)
+import asyncio
+from js import document, FileReader
+from pyodide import create_proxy
+
+
+async def process_file(event):
+    fileList = event.target.files.to_py()
+
+    for f in fileList:
+        # data = await f.text()
+        array_buf = Uint8Array.new(await f.arrayBuffer())
+        bytes_list = bytearray(array_buf)
+        data = io.BytesIO(bytes_list)
+        create_from_template(data)
+        # document.getElementById("input").innerHTML = data
+
+
+def main():
+    # Create a Python proxy for the callback function
+    # process_file() is your function to process events from FileReader
+    file_event = create_proxy(process_file)
+
+    # Set the listener to the callback
+    e = document.getElementById("input")
+    e.addEventListener("change", file_event, False)
+
+
+main()
+
+
+async def _upload_change_and_show(e):
+    # Get the first file from upload
+    file_list = e.target.files
+    first_item = file_list.item(0)
+
+    # Get the data from the files arrayBuffer as an array of unsigned bytes
+    array_buf = Uint8Array.new(await first_item.arrayBuffer())
+
+    # BytesIO wants a bytes-like object, so convert to bytearray first
+    bytes_list = bytearray(array_buf)
+    my_bytes = io.BytesIO(bytes_list)
+
+    # Create PIL image from np array
+    my_image = Image.open(my_bytes)
+
+    # Log some of the image data for testing
+    console.log(f"{my_image.format= } {my_image.width= } {my_image.height= }")
+
+    # Now that we have the image loaded with PIL, we can use all the tools it makes available.
+    # "Emboss" the image, rotate 45 degrees, fill with dark green
+    my_image = (
+        my_image.filter(ImageFilter.EMBOSS)
+        .rotate(45, expand=True, fillcolor=(0, 100, 50))
+        .resize((300, 300))
+    )
+
+    # Convert Pillow object array back into File type that createObjectURL will take
+    my_stream = io.BytesIO()
+    my_image.save(my_stream, format="PNG")
+
+    # Create a JS File object with our data and the proper mime type
+    image_file = File.new(
+        [Uint8Array.new(my_stream.getvalue())],
+        "new_image_file.png",
+        {type: "image/png"},
+    )
+
+    # Create new tag and insert into page
+    new_image = document.createElement("img")
+    new_image.src = window.URL.createObjectURL(image_file)
+    document.getElementById("output_upload_pillow").appendChild(new_image)
